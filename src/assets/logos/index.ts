07766dca2da5c4f7bb99488762f25bf644c18925
healer-1205 <crazyhealer1205@gmail.com>
2022-11-04 09:13:25 +0800

    remake from scratch with TypeScript

---
+++ src/assets/logos/index.ts
@@ -0,0 +1,8 @@
+import DarkLogo from "./dark-logo.png"
+import LightLogo from "./light-logo.png"
+const Logos = {
+  DarkLogo,
+  LightLogo,
+}
+
+export default Logos
---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,33 @@
+import darkHome from "./dark/home.svg"
+import darkCreate from "./dark/create.svg"
+import darkShare from "./dark/share.svg"
+import lightHome from "./light/home.svg"
+import lightCreate from "./light/create.svg"
+import lightShare from "./light/share.svg"
+import lightBeIcon from "./light/beIcon.svg"
+import lightFacebookIcon from "./light/facebookIcon.svg"
+import lightMsgIcon from "./light/msgIcon.svg"
+import lightTwitterIcon from "./light/twitterIcon.svg"
+import darkBeIcon from "./dark/beIcon.svg"
+import darkFacebookIcon from "./dark/facebookIcon.svg"
+import darkMsgIcon from "./dark/msgIcon.svg"
+import darkTwitterIcon from "./dark/twitterIcon.svg"
+
+const svgIcons = {
+  darkHome,
+  darkCreate,
+  darkShare,
+  lightHome,
+  lightCreate,
+  lightShare,
+  lightBeIcon,
+  lightFacebookIcon,
+  lightMsgIcon,
+  lightTwitterIcon,
+  darkBeIcon,
+  darkFacebookIcon,
+  darkMsgIcon,
+  darkTwitterIcon,
+}
+
+export default svgIcons
